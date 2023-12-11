0641c11880884b781bf1944df53d72e7d807f492
Your Name <you@example.com>
2023-12-11 17:26:21 +0300

    changing and responsive

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <main className="bg-white w-3/4 h-11/12 mx-auto flex flex-col justify-center mt-5 rounded-lg items-center">
+    <main className="bg-white md:w-3/4 h-11/12 mx-auto flex  md:flex-col justify-center mt-5 rounded-lg items-center">
       <Hero />
     </main>
   );
