a8a7d2f619b5758e7c40cc1e80fae0e4bebcb9db
Your Name <you@example.com>
2023-12-11 16:24:25 +0300

    filter function

--- components/messagePart.jsx
+++ components/messagePart.jsx
@@ -4,13 +4,14 @@ import React from "react";
 import ProjectData from "/assets/data/project.json";
 
 const MessagePart = () => {
+  const itemsLeft = ProjectData.filter((item) => item.completed).length;
   return (
     <div>
       <div className="flex justify-between w-full">
         <div className="flex gap-x-2 justify-center items-center">
           <h1 className="text-2xl font-bold">Notifications</h1>
           <div className="w-[35px] h-[35px] bg-[#0A317B] text-white flex justify-center items-center rounded-md font-bold">
-            0
+            {itemsLeft}
           </div>
         </div>
         <div className="flex justify-center items-center font-semibold">
